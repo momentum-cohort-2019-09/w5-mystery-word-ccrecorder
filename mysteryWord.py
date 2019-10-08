819ff558d660530dbee5ff82b18e3438cea447f0
Charles Wines <ccrecorder@gmail.com>
2019-10-08 14:43:59 -0400

    guess works

--- mysteryWord.py
+++ mysteryWord.py
@@ -3,9 +3,24 @@ import random
 with open("words.txt") as words_list:
   words = words_list.read().split()
 
-def random_word():
-  return random.choice(words)
-  
-  
-  
-print(random_word())
+answer = random.choice(words)
+answer_letters = list(answer)
+print("This word has", len(answer), "letters!")
+board = ['_ ' * len(answer)]
+print(board[0])
+
+wrong_guesses = 0
+letter_guesses = []
+word_guesses = []
+
+def compare_letter(guess):
+  wrong_guesses = 0
+  if guess in answer_letters:
+    print("that is good")
+  else:
+    wrong_guesses += 1
+    print("You wrong, you have", 8 - wrong_guesses, "guesses left!")
+    
+guess = input("guess a letter -->")
+
+compare_letter(guess)
